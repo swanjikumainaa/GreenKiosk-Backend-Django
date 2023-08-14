72ee351e5ea66feaa1f66a5d73aba8b0c96188db
swanjikumainaa <swanjikumainaa@gmail.com>
2023-08-14 21:21:55 +0300

    created templates and views for listing vendors,vendor details and edditting vendor on the seller app

--- seller/urls.py
+++ seller/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import register_vendor
+from .views import register_vendor,vendors_list,vendor_detail,edit_vendor_view
 
 urlpatterns =[
-path ("vendors/registration",register_vendor,name = "vendor_registration_view"),
+    path ("vendors/registration",register_vendor,name = "vendor_registration_view"),
+    path ("vendors/list",vendors_list,name = "vendors_list_view"),
+    path ("vendors/<int:id>/",vendor_detail,name = "vendor_detail_view"),
+    path ("vendor/edit/<int:id>/",edit_vendor_view,name="vendor_edit_view"),
+
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Vendor
+from django.shortcuts import redirect
+
 
 # Create your views here.
 from .forms import VendorRegistrationForm
@@ -11,4 +14,21 @@ def register_vendor(request):
     form = VendorRegistrationForm()
     return render(request,"seller/vendor_registration.html",{"form":form})
 
+def vendors_list(request):
+    vendors = Vendor.objects.all()
+    return render(request,"seller/vendors_list.html",{"vendors":vendors})
 
+def vendor_detail(request, id):
+    vendor = Vendor.objects.get(id=id)
+    return render(request,"seller/vendor_detail.html",{"vendor":vendor})
+
+def edit_vendor_view(request, id):
+    vendor = Vendor.objects.get(id=id)
+    if request.method == "POST":
+        form = VendorRegistrationForm(request.POST,instance=vendor)
+        if form.is_valid():
+            form.save()
+            return redirect("vendor_detail_view", id=vendor.id)
+    else:
+        form = VendorRegistrationForm(instance=vendor)
+        return render(request,"seller/edit_vendor.html",{"form":form})
